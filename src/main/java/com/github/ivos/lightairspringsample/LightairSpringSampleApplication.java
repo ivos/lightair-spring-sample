9b2ab31dcba8a7c5c98a7d4289ff1dbf9c6ae772
Ivo Maixner <ivo.maixner@gmail.com>
2019-12-30 13:58:49 +0100

    chore: Generate project stub from Spring Initializr

---
+++ src/main/java/com/github/ivos/lightairspringsample/LightairSpringSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.ivos.lightairspringsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightairSpringSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightairSpringSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ivos/lightairspringsample/LightairSpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ivos.lightairspringsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightairSpringSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
